47bc3664571bed4c7a161cd26f27b526164ccbf9
IWayanDivkaAstrada <127764669+IWayanDivkaAstrada@users.noreply.github.com>
2024-07-02 22:09:00 +0800

    first commit

---
+++ web/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratikum Docker</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #444;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            flex-direction: column;
+        }
+        .header {
+            background-color: #f4f4f4;
+            color: #000;
+            width: 100%;
+            padding: 20px 0;
+            text-align: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 1000;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        }
+        .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            margin-top: 80px; 
+            max-width: 600px;
+            width: 80%;
+            margin-bottom: 20px;
+            color: #000;
+        }
+        h1 {
+            font-size: 24px;
+            font-weight: bold;
+            margin: 0;
+        }
+        p {
+            font-size: 18px;
+            color: #000;
+            margin: 5px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>SISTEM TERDISTRIBUSI</h1>
+    </div>
+    <div class="container">
+        <h1>DOCKER</h1>
+        <p>NAMA: I WAYAN PUTRA DIVKA ASTRADA</p>
+        <p>NIM: 2215101031</p>
+        <p>KELAS: A</p>
+    </div>
+</body>
+</html>
